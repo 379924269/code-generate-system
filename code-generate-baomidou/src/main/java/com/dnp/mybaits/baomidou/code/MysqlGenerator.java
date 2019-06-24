d99b23a6239b07c278e5ed4a146a34f61868ea52
huazai <379924269@qq.com>
2019-06-24 14:11:02 +0800

    代码自动生成改了一点

--- code-generate-baomidou/src/main/java/com/dnp/mybaits/baomidou/code/MysqlGenerator.java
+++ code-generate-baomidou/src/main/java/com/dnp/mybaits/baomidou/code/MysqlGenerator.java
@@ -1,8 +1,6 @@
 package com.dnp.mybaits.baomidou.code;
 
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
@@ -12,7 +10,6 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * <p>
@@ -23,25 +20,28 @@ import java.util.Scanner;
  * @since 2018-09-12
  */
 public class MysqlGenerator {
+    /*作者*/
+    private static String AUTHOR = "huazai";
+    /*数据库用户名称*/
+    private static String DB_USER_NAME = "root";
+    /*数据库密码*/
+    private static String DB_PASSWORD = "123456";
+    /*数据库url*/
+    private static String DB_URL = "jdbc:mysql://localhost:3306/ms_ptt?useUnicode=true&useSSL=false&characterEncoding=utf8";
+    /*数据库driver*/
+    private static String DB_DRIVER = "com.mysql.jdbc.Driver";
 
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public static String scanner(String tip) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotEmpty(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
-    }
+    /*这个是当前项目的的绝对路径（开启的那个项目）， 这个地方我一般写的是项目的绝对路径：如：G:\my-git-project\code-generate-system\code-generate-baomidou*/
+    private static String PROJECT_PATH = System.getProperty("user.dir");
+    /*是否是子项目，是就填上，不是就填空。注意：不要少了前面那一个个分隔符*/
+    private static String SBU_PROJECT_NAME = "/common-permission-api";
+    /*自己定义的包名称*/
+    private static String PK_NAME = "com.dnp.ptt";
+    /* 自己定义的基本的操作生成到那个模块，没有就填空*/
+    private static String MODULE = "modular";
+    /*自定义要生成的信息表*/
+    private static String[] INCLUED_TABLE = {"users", "manager", "users_role", "manager_role", "resource", "role_resource"};
+
 
     /**
      * RUN THIS
@@ -52,26 +52,27 @@ public class MysqlGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/mybatis-plus-sample-generator/src/main/java");
-        gc.setAuthor("huazai");
+        gc.setOutputDir(PROJECT_PATH + SBU_PROJECT_NAME + "/src/main/java");
+        gc.setAuthor(AUTHOR);
         gc.setOpen(false);
         gc.setSwagger2(true);
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/ptt_jun_dui?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl(DB_URL);
         // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("123456");
+        dsc.setDriverName(DB_DRIVER);
+        dsc.setUsername(DB_USER_NAME);
+        dsc.setPassword(DB_PASSWORD);
         mpg.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名: "));
-        pc.setParent("com.baomidou.mybatisplus.samples.generator");
+//        模块名称根据自己的项目自己定义
+        pc.setModuleName(MODULE);
+//        pc.setParent("com.baomidou.mybatisplus.samples.generator");
+        pc.setParent(PK_NAME);
         mpg.setPackageInfo(pc);
 
         // 自定义配置
@@ -86,7 +87,7 @@ public class MysqlGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输入文件名称
-                return projectPath + "/mybatis-plus-sample-generator/src/main/resources/mapper/" + pc.getModuleName()
+                return PROJECT_PATH + SBU_PROJECT_NAME + "/src/main/resources/mapper/" + pc.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
@@ -98,10 +99,12 @@ public class MysqlGenerator {
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setSuperEntityClass("com.baomidou.mybatisplus.samples.generator.common.BaseEntity");
+//        strategy.setSuperEntityClass("com.baomidou.mybatisplus.samples.generator.common.BaseEntity");
         strategy.setEntityLombokModel(true);
-        strategy.setSuperControllerClass("com.baomidou.mybatisplus.samples.generator.common.BaseController");
-        strategy.setInclude(scanner("表名"));
+//        strategy.setSuperControllerClass("com.baomidou.mybatisplus.samples.generator.common.BaseController");
+        if (INCLUED_TABLE.length != 0) {
+            strategy.setInclude(INCLUED_TABLE); // 需要生成的表,注释就生成所有
+        }
         strategy.setSuperEntityColumns("id");
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
