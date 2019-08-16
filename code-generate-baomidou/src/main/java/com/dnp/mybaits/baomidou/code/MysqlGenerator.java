e5e8d77b9af8b358e639952c6f03beb3c1113519
huazai <379924269@qq.com>
2019-08-16 11:23:17 +0800

    1、修改：自动生成代码的@controller通过策略设置成了@restController 2、添加：把baomidou包里面的controller.java.ftl 改了一下requestmapping中要用到模块名，去除掉了。新的代码生成用的ftl文件

--- code-generate-baomidou/src/main/java/com/dnp/mybaits/baomidou/code/MysqlGenerator.java
+++ code-generate-baomidou/src/main/java/com/dnp/mybaits/baomidou/code/MysqlGenerator.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,8 +38,8 @@ public class MysqlGenerator {
     /*自己定义的包名称*/
     private static String PK_NAME = "com.dnp.ptt";
     /* 自己定义的基本的操作生成到那个模块，没有就填空*/
-    private static String MODULE = "modular";
-    /*自定义要生成的信息表*/
+    private static String MODULE = "system";
+    /*自定义要生成的信息表,不传生成所有表*/
     private static String[] INCLUED_TABLE = {"users", "manager", "users_role", "manager_role", "resource", "role_resource"};
 
 
@@ -47,7 +48,7 @@ public class MysqlGenerator {
      */
     public static void main(String[] args) {
         // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
+        AutoGenerator autoGenerator = new AutoGenerator();
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
@@ -55,7 +56,7 @@ public class MysqlGenerator {
         gc.setAuthor(AUTHOR);
         gc.setOpen(false);
         gc.setSwagger2(true);
-        mpg.setGlobalConfig(gc);
+        autoGenerator.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
@@ -64,18 +65,20 @@ public class MysqlGenerator {
         dsc.setDriverName(DB_DRIVER);
         dsc.setUsername(DB_USER_NAME);
         dsc.setPassword(DB_PASSWORD);
-        mpg.setDataSource(dsc);
+        autoGenerator.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
 //        模块名称根据自己的项目自己定义
-        pc.setModuleName(MODULE);
+        if (StringUtils.isNotEmpty(MODULE)) {
+            pc.setModuleName(MODULE);
+        }
 //        pc.setParent("com.baomidou.mybatisplus.samples.generator");
         pc.setParent(PK_NAME);
-        mpg.setPackageInfo(pc);
+        autoGenerator.setPackageInfo(pc);
 
         // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
+        InjectionConfig injectionConfig = new InjectionConfig() {
             @Override
             public void initMap() {
                 // to do nothing
@@ -90,9 +93,9 @@ public class MysqlGenerator {
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-        mpg.setTemplate(new TemplateConfig().setXml(null));
+        injectionConfig.setFileOutConfigList(focList);
+        autoGenerator.setCfg(injectionConfig);
+        autoGenerator.setTemplate(new TemplateConfig().setXml(null));
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
@@ -104,13 +107,14 @@ public class MysqlGenerator {
         if (INCLUED_TABLE.length != 0) {
             strategy.setInclude(INCLUED_TABLE); // 需要生成的表,注释就生成所有
         }
-        strategy.setSuperEntityColumns("id");
-        strategy.setControllerMappingHyphenStyle(true);
+//        strategy.setSuperEntityColumns("id");
+        strategy.setRestControllerStyle(true);
+        strategy.setControllerMappingHyphenStyle(false);
         strategy.setTablePrefix(pc.getModuleName() + "_");
-        mpg.setStrategy(strategy);
+        autoGenerator.setStrategy(strategy);
         // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+        autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
+        autoGenerator.execute();
     }
 
 }
